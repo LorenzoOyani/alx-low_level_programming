a454337780322e9961852abb390832c956b7b473
Jerumeh <jerumehlawrence@gmail.com>
2022-10-14 07:32:11 -0700

    new changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,18 @@
-#!/bin/bash
  #include <stdio.h>
 
 
 
 /**
+ *   main - Entry point
  *
- *  * main - Entry point
- *
- *   * Return: Always 0 (Success)
- *
- *    */
+ *   Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
 
 }
 
