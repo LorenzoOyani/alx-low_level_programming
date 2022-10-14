eb3a211df62b91fcd85f9786af4fe305e7617cf9
Jerumeh <jerumehlawrence@gmail.com>
2022-10-14 09:44:17 -0700

    new changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,20 @@
+
+
 #include <stdio.h>
 
-
-
-
+#include <unistd.h>
 
 /**
- *   	main - Entry point
- *
- *	Return: Always 0 (Success)
+ *  main - Entry point
+ *  Return:1
  */
 
 int main(void)
 
 {
 
-		fprintf(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-		return (1);
-
-
+			return (1);
 
 }
