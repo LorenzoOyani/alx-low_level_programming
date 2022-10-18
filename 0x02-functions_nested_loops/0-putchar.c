fea9f6e235290f0b175e315411594ba6483dd7c1
Jerumeh lawrence <jerumehlawrence@gmail.com>
2022-10-18 21:50:18 +0100

    new changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,15 +11,15 @@
 int main(void)
 {
 	char s[] = "_putchar";
-        int i = 0;
+	int i = 0;
 
 	while(s[i] != '\0')
 	{
-		_putchar(s[i]);
+		putchar(s[i]);
 		i++;
 	}	
 	_putchar(10);
-	
-	return (0);	
+
+	return (0);
 
 }	
