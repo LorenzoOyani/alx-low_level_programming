e39f8a188e33ace3ac4922645b5f939618532ddd
Jerumeh lawrence <jerumehlawrence@gmail.com>
2022-10-19 23:33:07 +0100

    new

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * * print_times_table - Print the n times table
@@ -26,19 +26,19 @@ void print_times_table(int n)
 		{
 		_putchar(32);
 		_putchar(32);
-		_putchar(op + 48);
+		_putchar(op + 48)
 		}
 		else if (op <= 99)
 		{
 		_putchar(32);
 		_putchar((op / 10) + 48);
-		_putchar((op % 10) + 48);
+		_putchar((op / 10) + 48);
 		}
 		else
 		{
 		_putchar(((op / 100) % 10) + 48);
 		_putchar(((op / 10) % 10) + 48);
-		_putchar((op % 10) + 48);
+		_putchar(((op % 10) + 48));
 		}
 	}
 	_putchar('\n');
