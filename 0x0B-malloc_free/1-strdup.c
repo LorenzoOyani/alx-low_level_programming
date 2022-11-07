de079f108e9e610857317db2429b995b7eb1ced9
Jerumeh lawrence <jerumehlawrence@gmail.com>
2022-11-08 00:15:08 +0100

    updated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -22,8 +22,8 @@ char *_strdup(char *str)
 
     if(dup ==NULL)
         return (NULL);
-        
-    while (dup[i] == str[i] != '\0')
+
+    while ((dup[i] == str[i] != '\0'))
             i++;
     return (dup);        
 }
