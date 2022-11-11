c6f244ace353c4777249b6140becb6642621cf65
Jerumeh lawrence <jerumehlawrence@gmail.com>
2022-11-11 01:56:57 +0100

    updated

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,12 +7,12 @@
  * Return: pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
  */
-
 void *malloc_checked(unsigned int b)
 {
-        char *p;
-        p = malloc(b);
-        if(p = NULL)
-                exit(98);
-        return (p);        
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
