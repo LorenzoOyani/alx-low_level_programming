c91b70f2df1d0ce6175f3bd4464ba82a0b6d9652
Jerumeh lawrence <jerumehlawrence@gmail.com>
2022-11-26 00:28:34 +0100

    updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,23 +9,23 @@
  * you are allowed to use printf
  */
 
-
-
-    
-
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t count = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
-		nelem++;
+		count++;
 	}
-	return (nelem);
+
+	return (count);
 }
+
+    
+
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,33 +10,34 @@
  * Return: address of the head.
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+ list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new, *temp;
-    size_t nchar;
+	list_t *new, *temp;
+	size_t nchar;
 
-    new = malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-    if (new == NULL)
-            return (NULL);
+	new->str = strdup(str);
 
-    new -> str = strdup(str);
-    new ->next = NULL;
-    temp  = *head;
+	for (nchar = 0; str[nchar]; nchar++)
+		;
 
-    if( temp == NULL)
-    {
-        *head = new;
-    }
-    else
-    {
-        while(temp != NULL)
-        
-            temp = temp -> next;
+	new->len = nchar;
+	new->next = NULL;
+	temp = *head;
 
-        temp -> next = new;    
-        
-        temp ->next = *head;
-    } 
-    return (*head);       
-}   
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
+
+	return (*head);
+}  
