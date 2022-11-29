6a3681cc6f9e624cab40cd87f941a024fb7aa32a
Jerumeh lawrence <jerumehlawrence@gmail.com>
2022-11-30 00:29:26 +0100

    updated

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -13,7 +13,7 @@ size_t listint_len(const listint_t *h)
     while(h)
     {
         nodenum++;
-        h = h -> next;
+        h = h ->next;
     }
     return(nodenum);
 }
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -7,8 +7,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
     listint_t *new;
     new  = malloc(sizeof(listint_t));
 
+    if(!new)
+            return NULL;
     new ->n = n;
-    new -> next = (*head);
+    new -> next = *head;
 
-    (*head) = new;
+    *head = new;
+    return (*head);
 }
