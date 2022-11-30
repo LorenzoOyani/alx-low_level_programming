078fe25aab0ffbe0ec9f04762444dcb922838d08
Jerumeh lawrence <jerumehlawrence@gmail.com>
2022-11-30 01:09:28 +0100

    updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
     if(head ==NULL)
             return (NULL);
 
-    while(*head)
+    while(*head ==NULL)
     {
         temp = (*head) -> next;
         free(*head);
